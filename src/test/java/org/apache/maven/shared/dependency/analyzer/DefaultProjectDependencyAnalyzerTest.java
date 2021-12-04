a5fb9b61df0b78d62eea9107596eb733a1076ff3
Joseph Walton <joe@kafsemo.org>
2021-12-05 00:51:10 +1100

    [MDEP-779] Test that classes are returned.
    Add testing for specific class names to the case for used,
    undeclared dependencies.

--- src/test/java/org/apache/maven/shared/dependency/analyzer/DefaultProjectDependencyAnalyzerTest.java
+++ src/test/java/org/apache/maven/shared/dependency/analyzer/DefaultProjectDependencyAnalyzerTest.java
@@ -41,6 +41,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
@@ -435,6 +436,11 @@ public class DefaultProjectDependencyAnalyzerTest
         Set<Artifact> expectedUsedUndeclaredArtifacts = Collections.singleton( xmlApis );
 
         assertEquals( expectedUsedUndeclaredArtifacts, actualAnalysis.getUsedUndeclaredArtifacts() );
+
+        Map<Artifact, Set<String>> expectedUsedUndeclaredArtifactsWithClasses =
+                Collections.singletonMap(xmlApis, Collections.singleton("org.apache.xmlcommons.Version") );
+
+        assertEquals( expectedUsedUndeclaredArtifactsWithClasses, actualAnalysis.getUsedUndeclaredArtifactsWithClasses() );
     }
 
     // private methods --------------------------------------------------------
